ab8b3af6e8511a2c5144530c900f9a2dc4956342
unknown <>
2019-08-03 00:48:39 +0800

    sms自动生成代码

--- gulimall-oms/src/main/java/com/atguigu/gulimall/oms/GulimallOmsApplication.java
+++ gulimall-oms/src/main/java/com/atguigu/gulimall/oms/GulimallOmsApplication.java
@@ -4,8 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
+@EnableSwagger2
 @MapperScan(basePackages = "com.atguigu.gulimall.oms.dao")
 @SpringBootApplication
 public class GulimallOmsApplication {
